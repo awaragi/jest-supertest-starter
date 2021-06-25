f64c6771e7bf33bd5281b4696e43bbcb79b5aae6
Pierre Awaragi <pierre@awaragi.com>
2021-06-24 23:16:22 -0400

    added login to test case

--- src/test/register.feature/register.spec.js
+++ src/test/register.feature/register.spec.js
@@ -7,24 +7,27 @@ const RegisterApi = require("../../api/register.api");
 let anonymous;
 let token;
 let authenticated;
+let authApi;
 let usersApi;
 let registerApi;
 beforeAll(async () => {
   anonymous = new Client();
   registerApi = new RegisterApi(anonymous);
-  const auth = new AuthApi(anonymous);
-  token = await auth.login(process.env.AUTH_EMAIL, process.env.AUTH_PASSWORD);
+  authApi = new AuthApi(anonymous);
+  token = await authApi.login(process.env.AUTH_EMAIL, process.env.AUTH_PASSWORD);
   authenticated = new ClientAuthenticated(token);
   usersApi = new UserApi(authenticated);
 });
 
 describe("1. User Registration API", () => {
-  it("1. should be able to register and be part of system", async () => {
+  it("1. should be able to register and be part of system and login", async () => {
     let email = "eve.holt@reqres.in";
     let password = "pistol";
 
     const { id } = await registerApi.register(email, password);
     let user = await usersApi.findById(id);
     expect(user.email).toBe(email);
+    const token = await authApi.login(email, password);
+    expect(token.length).toBeGreaterThan(0);
   });
 });
